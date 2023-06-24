6620ab852918afbc00b95019abb981cd1f0095f6
weishanteam <132208199+weishanteam@users.noreply.github.com>
2023-06-24 13:32:52 +0800

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,5 +1,5 @@
 local a = identifyexecutor()
-if a == "Fluxus" then
+if a == "Fluxus Android" then
   loadstring(game:HttpGet"https://raw.githubusercontent.com/weishanteam/New-delta/main/bai.lua")()
 else
   loadstring(game:HttpGet(('https://raw.githubusercontent.com/wev666666/baijiaobengV2.0beta/main/%E7%99%BD%E8%84%9A%E6%9C%ACbeta'),true))()
