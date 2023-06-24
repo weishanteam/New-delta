89d254d79006006a557c38a00a0f00350aaa54d3
weishanteam <132208199+weishanteam@users.noreply.github.com>
2023-06-24 13:26:19 +0800

    Create check.lua

---
+++ check.lua
@@ -0,0 +1,7 @@
+local a = identifyexecutor()
+if a == "Fluxus" then
+  loadstring(game:HttpGet"https://raw.githubusercontent.com/weishanteam/New-delta/main/bai.lua")()
+else
+  loadstring(game:HttpGet(('https://raw.githubusercontent.com/wev666666/baijiaobengV2.0beta/main/%E7%99%BD%E8%84%9A%E6%9C%ACbeta'),true))()
+end
+print("微山工作室")
