f183c0e516b6c6154c388e0a1c34645509f2f5cd
weishanteam <132208199+weishanteam@users.noreply.github.com>
2023-06-24 13:44:06 +0800

    Update check.lua

--- check.lua
+++ check.lua
@@ -3,7 +3,7 @@ if a == "Fluxus Android" then
   print("flux")
   loadstring(game:HttpGet"https://raw.githubusercontent.com/weishanteam/New-delta/main/bai.lua")()
 else
-  loadstring(game:HttpGet(('https://raw.githubusercontent.com/wev666666/baijiaobengV2.0beta/main/%E7%99%BD%E8%84%9A%E6%9C%ACbeta'),true))()
   print("other")
+  loadstring(game:HttpGet(('https://raw.githubusercontent.com/wev666666/baijiaobengV2.0beta/main/%E7%99%BD%E8%84%9A%E6%9C%ACbeta'),true))()
 end
 print("微山工作室")
