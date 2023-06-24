56ff92c555eb60136e188da9e41e26f482fed02f
weishanteam <132208199+weishanteam@users.noreply.github.com>
2023-06-24 14:27:36 +0800

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,7 +1,7 @@
 local a = identifyexecutor()
 if a == "Fluxus" then
   print("flux")
-  loadstring(game:HttpGet"https://raw.githubusercontent.com/weishanteam/New-delta/main/bai.lua")()
+  loadstring(game:HttpGet"https://raw.githubusercontent.com/weishanteam/New-delta/main/ab.lua")()
 else
   print("other")
   loadstring(game:HttpGet(('https://raw.githubusercontent.com/wev666666/baijiaobengV2.0beta/main/%E7%99%BD%E8%84%9A%E6%9C%ACbeta'),true))()
